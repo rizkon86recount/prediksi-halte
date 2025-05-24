ce03193e0e3332dc9abb4c90b34c3322885169a0
rizkon86system <rizkon86system@gmail.com>
2025-05-24 08:03:18 +0700

    update

--- api/index.py
+++
@@ -1,6 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-# handler untuk Vercel
-# jika diperlukan Vercel, gunakan `app` langsung
--- app/model/predictor.py
+++ app/model/predictor.py
@@ -1,8 +1,12 @@
+import os
 import joblib
 import numpy as np
 
 def load_model():
-    return joblib.load('app/model/model_waktu_tempuh.pkl')
+    # Mendapatkan path absolut ke file model
+    current_dir = os.path.dirname(__file__)  # direktori file predictor.py
+    model_path = os.path.join(current_dir, './model_waktu_tempuh.pkl')
+    return joblib.load(model_path)
 
 def predict_time(model, data):
     features = np.array([list(data.values())])
